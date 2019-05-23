91ddbb176887160f589023c51a262309058f4cc9
Nick Aja <ajanick3@gmail.com>
2019-05-23 07:39:52 -0400

    move functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const mailFunc = require('./mail')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
